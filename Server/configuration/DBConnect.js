b641512d68e333970df3533233028dc54fcfd7b6
KrimiWajih <krimi.wajih@gmail.com>
2025-05-24 22:33:38 +0100

    Update DBConnect.js

--- Server/configuration/DBConnect.js
+++ Server/configuration/DBConnect.js
@@ -6,7 +6,7 @@ const database = process.env.MONGO_URL
 exports.connectDB =async ()=>{
     try {
        await mongoose.connect(database)
-        console.log('Mongo URI on Render:', process.env.MONGO_URI);
+        console.log('Mongo URI on Render:', process.env.PORT);
         console.log("Connected to Database")
     } catch (error) {
         console.log('Mongo URI on Render:', process.env.MONGO_URI);
